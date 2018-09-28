83e6b3f67c031ee0ea5c601262ee73619f0a9555
Torsten Holva <torstenholva@users.noreply.github.com>
2018-09-28 13:13:46 +0300

    Fixed some TYPOs

--- predict_interface_usage.py
+++ predict_interface_usage.py
@@ -12,12 +12,15 @@ train = False if len(sys.argv) == 2 else sys.argv[2]
 
 ret_vals = predict_interface.pred_from_img(image, train)
 
+print(" ")
+print("----------")
+	
 for i in range(0, len(ret_vals)):
 	print("Prediction: " + str(ret_vals[i].get_prediction()) + ", Probability: " + str(ret_vals[i].get_probability()))
-	print("Location in image: x(" + str(ret_vals[i].get_location()[0]) + "), y(" + str(ret_vals[i].get_location()[1]) + ")")
-	print("Top left corner (shifted image): x(" + str(ret_vals[i].get_top_left()[0]) + "), y(" + str(ret_vals[i].get_top_left()[1]) + ")")
-	print("Bottom right corner (shifted image): x(" + str(ret_vals[i].get_bottom_right()[0]) + "), y(" + str(ret_vals[i].get_bottom_right()[1]) + ")")
-	print("Actual width and height (cropped image): w(" + str(ret_vals[i].get_actual_w_h()[0]) + "), h(" + str(ret_vals[i].get_actual_w_h()[1]) + ")")
+	print("Location in image: x(" + str(ret_vals[i].get_location()[1]) + "), y(" + str(ret_vals[i].get_location()[0]) + ")")
+	print("Top left corner (shifted image): x(" + str(ret_vals[i].get_top_left()[1]) + "), y(" + str(ret_vals[i].get_top_left()[0]) + ")")
+	print("Bottom right corner (shifted image): x(" + str(ret_vals[i].get_bottom_right()[1]) + "), y(" + str(ret_vals[i].get_bottom_right()[0]) + ")")
+	print("Actual width and height (cropped image): w(" + str(ret_vals[i].get_actual_w_h()[1]) + "), h(" + str(ret_vals[i].get_actual_w_h()[0]) + ")")
 	print(" ")
 	print("----------")
 
